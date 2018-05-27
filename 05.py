16a95d3786f833f96fc35e85b59723deb5deb633
Maroonlk <821365628@qq.com>
2018-05-28 01:49:58 +0800

    by pycharm 0526

--- 05.py
+++ 05.py
@@ -1,5 +1,3 @@
-
-
 class Student(object):
     count = 0
 
@@ -17,6 +15,7 @@ print(bob.name)
 
 print(Student.count)
 
+
 class SS():
     name = "None"
     age = 0
@@ -25,5 +24,26 @@ class SS():
         self.name = name
         self.age = age
 
+
 apple = SS("apple", 18)
 print(apple.name)
+
+
+class Mod(object):
+    # __slots__ = ("name", "age")   # 限制对象所能拥有的属性
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def say(self):
+        print("Hi, I am {0} and I'm {1} year old.".format(self.name, self.age))
+
+
+apple = Mod("apple", 18)
+
+apple.say()
+
+print(setattr(apple, "name2", "ap"))  # 设置一个"name"的属性，值为"ap"
+print(apple.name2)
+
