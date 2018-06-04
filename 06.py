9ab42ad5527e4b3f3a64a75337a2bd59cd7d8d1e
Maroonlk <821365628@qq.com>
2018-06-04 22:43:22 +0800

    by mac pycharm 0604

--- 06.py
+++ 06.py
@@ -1,23 +1,1 @@
-class Person(object):
-    name = "None"
-    age = 18
-    __score = 0  # 考试成绩是秘密，只要自己知道
-    _petname = "sec"  # 小名，受保护的，子类可以用，但不能共用
-
-    def sleep(self):
-        print("Sleeping... ...")
-
-
-class Teacher(Person):
-    teacher_id = "9527"
-
-    def make_test(self):
-        print("Make_test... ...")
-
-
-t = Teacher()
-print(t.name)
-print(t._petname)
-# print(t.__score)
-t.sleep()
-t.make_test()
+class Person(object):    name = "None"    age = 18    __score = 0  # 考试成绩是秘密，只要自己知道    _petname = "sec"  # 小名，受保护的，子类可以用，但不能共用    def sleep(self):        print("Sleeping... ...")class Teacher(Person):    teacher_id = "9527"    name = "Bob"    def make_test(self):        print("Make_test... ...")t = Teacher()print(t.name)print(t._petname)# print(t.__score)t.sleep()t.make_test()
