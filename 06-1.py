66d5af719373f1e405d9955d94c6cc596ffd3663
Maroonlk <821365628@qq.com>
2018-06-06 19:18:07 +0800

    by mac pycharm 0606

--- 06-1.py
+++ 06-1.py
@@ -2,5 +2,41 @@ class Animel(object):
     def __init__(self, name):
         print("I am Animel {0}".format(name))
 
-class Dog(Animel):
-    super().__init__()
+
+
+class Fish():
+    def __init__(self, name):
+        self.name = name
+
+    def swim(self):
+        print("I am swimming......")
+
+class Bird():
+    def __int__(self, name):
+        self.name = name
+
+    def fly(self):
+        print("I am flying.....")
+
+class Person():
+    def __init__(self, name):
+        self.name = name
+
+    def work(self):
+        print("I am working.....")
+
+
+
+class SuperMan(Animel, Person, Bird, Fish):
+    pass
+
+class Student(Person):
+    pass
+
+apple = SuperMan("apple")
+apple.work()
+apple.fly()
+apple.swim()
+
+bob = Student("bob")
+bob.work()
