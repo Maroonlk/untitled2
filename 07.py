4a727697699a442216ef54a14f82852ae129fca2
Maroonlk <821365628@qq.com>
2018-06-06 22:48:29 +0800

    by win10 pycharm 0606

--- 07.py
+++ 07.py
@@ -1,11 +1,22 @@
 class A():
-    pass
+    def __init__(self, name):
+        print("A", name)
 
 class B(A):
-    pass
+    def __init__(self, name, age):
+        A.__init__(self, name)
+        print("B", age)
 
-class C(A):
-    pass
+class C():
+    def __init__(self, name, work):
+        print("C", name, work)
 
-class D(B, C):
-    pass
+class D(C):
+    def __init__(self, name, work, age):
+        super(D, self).__init__(name, work)
+        print("D", age)
+
+
+apple = B("apple", 18)
+bob = D("bob", "eat", 21)
+
---
+++ 08-Mixin.py
@@ -0,0 +1,44 @@
+# Mixin案例
+
+class Person():
+    name = "apple"
+    age = 18
+
+    def eat(self):
+        print("EAT......")
+
+    def drink(self):
+        print("DRINK......")
+
+    def sleep(self):
+        print("SLEEP......")
+
+class Teacher(Person):
+    def work(self):
+        print("work")
+
+class Student(Person):
+    def study(self):
+        print("study")
+
+class Tutor(Teacher, Student):
+    pass
+
+
+class TeacherMixin():
+    def work(self):
+        print("work")
+
+class StudentMixin():
+    def study(self):
+        print("study")
+
+
+class TutorMixin(Person, TeacherMixin, StudentMixin):
+    pass
+
+t = TutorMixin()
+
+print(Tutor.__mro__)
+print(t.__dict__)
+print(Tutor.__dict__)
