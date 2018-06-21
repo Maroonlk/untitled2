8a955ffa46b03a70d73b527689e3cf19cdd0e3d8
Maroonlk <821365628@qq.com>
2018-06-21 20:50:28 +0800

    by mac pycharm

--- 练习题/优惠码.py
+++ 练习题/优惠码.py
@@ -1,13 +1,15 @@
 import random
 '''
-此程序生成一个4 x 4的包含阿拉伯数字和26个不区分大小写字母的激活码
+此程序生成一个4 x 5的包含阿拉伯数字和26个不区分大小写字母的激活码
 例： 12Df7-V8Fd1-mg8Yr-s9IkJ 
 '''
 class JiHuoMa(object):
-    def __init__(self, ma=None, shi_yong="No"):
-        self.__ma = JiHuoMa.random_ma(self)
+    def __init__(self, ma=None, shi_yong="used"):
+        self.__ma = JiHuoMa.random_ma()
         self.__zhuang_tai = shi_yong
-        return "add is ok"
+
+    def __str__(self):
+        return self.ma
 
     @property
     def ma(self):
@@ -26,25 +28,31 @@ class JiHuoMa(object):
     zt = property(fget=get_zt, fset=set_zt, fdel=None, doc=None)
     # 获取和设置激活码是否被使用的状态
 
-    def random_ma1(self):
-        # 随即其中一组激活码
+    @staticmethod
+    def random_ma1():
+        # 随机其中一组激活码
         assi = ("a", "b", "c", "d", "e", "f", "g",\
                 "h", "i", "j", "k", "l", "m", "n",\
                 "o", "p", "q", "r", "s", "t", "u",\
                 "v", "w", "x", "y", "z", "1", "2",\
                 "3", "4", "5", "6", "7", "8", "9"\
                 )
-        ma_1 = [x+y+z+c for x in random.choice(assi) for y in random.choice(assi)\
-                for z in random.choice(assi) for c in random.choice(assi)]
+        ma_1 = [x+y+z+c+v+"-" for x in random.choice(assi) for y in random.choice(assi)\
+                for z in random.choice(assi) for c in random.choice(assi) for v in random.choice(assi)]
         
         return ma_1
 
-    def random_ma(self):
-        return JiHuoMa.random_ma1(self) + "-" + JiHuoMa.random_ma1(self) + "-" + \
-               JiHuoMa.random_ma1(self) + "-" + JiHuoMa.random_ma1(self)
+    @staticmethod
+    def random_ma():
+        ma = JiHuoMa.random_ma1() + JiHuoMa.random_ma1() + \
+             JiHuoMa.random_ma1() + JiHuoMa.random_ma1() + \
+             JiHuoMa.random_ma1()
+        return ''.join(ma)[:-1].upper()
+        # 组成4x4的激活码，以“-”号分割，且去掉最后一个“-”
 
-j1 = JiHuoMa()
 
-print(j1.ma())
-print(j1.zt())
+if __name__ == '__main__':
+    j1 = JiHuoMa()
+    print(j1.ma)
+    print(j1.zt)
 
