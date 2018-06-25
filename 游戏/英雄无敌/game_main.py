c224fe20cfc44304db3c7612fb05bcec1355e721
Maroonlk <821365628@qq.com>
2018-06-25 22:32:56 +0800

    by win10 pycharm

--- 游戏/英雄无敌/game_main.py
+++ 游戏/英雄无敌/game_main.py
@@ -4,18 +4,43 @@ Maroonlk
 2018-06-25
 '''
 
+class GameMap(object):
+    def __init__(self, size=[4, 4]):
+        self.size = size
+        self.map = [[x,y] for x in range(0,size[0]) for y in range(0,size[1])]
 
-class Person(object):
-    def __init__(self, name='NoName', gong_ji=5, xue_liang=100, fang_yu=0):
+    def pri_Map(self):
+        for x,y in self.map:
+            print("#", end='')
+            if y == self.size[1]-1:
+                print("")
+
+
+map1 = GameMap()
+map1.pri_Map()
+
+
+
+class User(object):
+    def __init__(self, name='Player01', gong_ji=5, xue_liang=100, fang_yu=0, wei_zhi=[0,0]):
         self.name = name
         self._gong_ji_li = gong_ji
         self._xue_liang = xue_liang
         self._fang_yu = fang_yu
+        self._wei_zhi = wei_zhi
 
-    def gong_Ji(self, who, value=who._gong_ji_li):
+    def shou_Shang(self, who, value):
         self._xue_liang -= (value - self._fang_yu / 2)
 
-        print('{0}受到了来自{1}的{2}点伤害！'.format(self.name, who, value)
-        print('{0}剩余HP:  {1}'.format(self.name, self._xue_liang)
+        if self._xue_liang > 0:
+            print(" |{0}| 受到了 |{1}| 的 ~{2}~ 点伤害".format(self.name, who, value))
+            print(" |{0}| 剩余HP: ~{1}~".format(self.name, self._xue_liang))
+        else:
+            print("GameOver!")
+            return -1
 
-    def go_move(self):
+    def move_User(self, fang_xiang):
+        if fang_xiang == 'a' and self._wei_zhi[0] != 0:
+            self._wei_zhi[0] -= 1
+        if fang_xiang == 'd' and self._wei_zhi[0] != GameMap.size[1]:
+            self._wei_zhi[0] += 1
